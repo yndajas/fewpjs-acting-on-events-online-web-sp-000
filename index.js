5e6e794ff56b339de6d362c4c1e9cc17913ec076
Ynda Jas <yndajas@gmail.com>
2021-03-17 03:11:00 +0000

    commit tree 139ff92a54cbed71d51f350ef419ff2d11c2b6e0

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ function moveDodgerLeft() {
 }
 
 // my solution which works (and accounts for the right edge) but for some reason doesn't pass the test:
-// function moveDodgerRight() {
-//     let leftNumbers = dodger.style.left.replace("px", "");
-//     let left = parseInt(leftNumbers, 10);
-//     let width = dodger.offsetWidth;
-//     let right = left + width;
-//     let parentWidth = dodger.parentNode.offsetWidth;
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    let width = dodger.offsetWidth;
+    let right = left + width;
+    let parentWidth = dodger.parentNode.offsetWidth;
 
-//     if (right < parentWidth) {
-//       dodger.style.left = `${left + 1}px`;
-//     }
-// }
+    if (right < parentWidth - 1) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
 
 // Flatiron solution that passes the test (but doesn't account for the right edge):
-function moveDodgerRight() {
-    var leftNumbers = dodger.style.left.replace('px', '');
-    var left = parseInt(leftNumbers, 10)
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`
-    }
-  }
+// function moveDodgerRight() {
+//     var leftNumbers = dodger.style.left.replace('px', '');
+//     var left = parseInt(leftNumbers, 10)
+//     if (left > 0) {
+//       dodger.style.left = `${left + 1}px`
+//     }
+//   }
   
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
